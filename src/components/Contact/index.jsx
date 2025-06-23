38bd286018262222a82984b5f60252f9591ed4a0
Jackie Lin <JLin226859643@gmail.com>
2025-06-23 13:59:55 -0400

    contact

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,84 @@
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { useEffect, useRef, useState } from 'react'
+import Loader from 'react-loaders'
+import emailjs from '@emailjs/browser'
+
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_x4qllbo',     
+        'template_fght8m9',    
+        form.current,
+        'IMoLInR98Y31cPCEt'      
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        (error) => {
+          alert('Failed to send the message, please try again.')
+          console.error(error)
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              letterClass={letterClass}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm currently looking for internship opportunities where I can contribute, learn, and grow as a developer.
+            Feel free to reach out if you think I’d be a good fit for your team!
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input type="email" name="email" placeholder="Email" required />
+                </li>
+                <li>
+                  <input type="text" name="subject" placeholder="Subject" required />
+                </li>
+                <li>
+                  <textarea name="message" placeholder="Message" required></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
